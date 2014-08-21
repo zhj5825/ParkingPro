c546559202434acb8479bf54eda8b634d9c00714
Ziming Zhao <ziming.zhao@gmail.com>
2014-08-20 21:45:10 -0700

    add google-play-services_lib for Google Maps

--- AndroidApp/src/com/yxzz/parkingpro/activities/MainActivity.java
+++ AndroidApp/src/com/yxzz/parkingpro/activities/MainActivity.java
@@ -3,38 +3,7 @@ package com.yxzz.parkingpro.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MainActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
