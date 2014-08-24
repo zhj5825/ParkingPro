ed268efe88cbe763d59ab06325cec52d7f7bc28a
fangxi.cn@gmail.com <fangxi.cn@gmail.com>
2014-08-23 22:33:44 -0700

    Added a test demo for PHP restApi

--- Server/def.php
+++ Server/def.php
@@ -1,8 +1,13 @@
 <?php
 /********************
-mPayment System Prototype
-
-Ziming Zhao 
-GFS Technology Inc.
+DEMO: replace the port 8014 with your local host port
 ********************/
+$myString = "Hello!";
+echo $myString;
+echo "<h5>I love using ParkingPro!</h5>";
+
+$app_info = file_get_contents('http://localhost:8014/ParkingPro/Server/restapidemo/testdemoapi.php?action=get_app_list');
+$app_info = json_decode($app_info, true);
+var_dump($app_info);
+;
 ?>
