a67b847fc96915c38773782de35840e0480b34cb
fangxi.cn@gmail.com <fangxi.cn@gmail.com>
2014-09-28 11:49:26 -0700

    finish the code, but not tested

--- Server/db/DBConnection.php
+++ Server/db/DBConnection.php
@@ -1,36 +1,54 @@
 <?php
-class DBConnection
-{
-	public $m_con;
-	
-	public function __construct()
-	{
-		$this->m_con = false;
-	}
-	
-	public function __destruct()
-	{
-		if ($this->m_con)
-		{
-			//mysql_close($this->m_con);
-			//$this->m_con = false;
-			//debug_print_backtrace();
-		}
-	}
-	
-	public function connect()
-	{
-		$this->m_con = mysql_connect("localhost","root","");
-		
-		if (!$this->m_con)
-		{
-			//die('Could not connect: ' . mysql_error());
-		}
-		
-		if (!mysql_select_db("mpaymentdb", $this->m_con))
-		{
-			//die('Could not find db: ' . mysql_error());
-		}
-	}
+
+require ("configuration.php");
+
+/**
+ * DB connection class.
+ * TODO(xifang): Add logic for retry when connection is down.
+ *
+ */
+class DBConnection {
+
+    private $connection;
+
+    public function __construct() {
+        $this->connection = null;
+    }
+
+    public function __destruct() {
+        close_db();
+    }
+
+    public function connect_localhost() {
+        $this->connection = connect_db(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+    }
+
+    public function connect_db($host, $user, $password, $dbname) {
+        $this->connection = mysql_connect($host, $$user, $password, $dbname);
+
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            $this->connection = null;
+        }
+    }
+
+    public function close_db() {
+        if ($this->connection == null) {
+            echo "No connection to MySQL: " . mysqli_connect_error();
+            return;
+        }
+        mysqli_close($this->connection);
+        $this->connection = null;
+    }
+
+    public function execute_sql_query($sqlquery) {
+        $result = mysql_query($sqlquery, $this->connection);
+        if ($result == false) {
+            echo "No connection to MySQL: " . mysql_error();
+        }
+        return $result;
+    }
+
 }
+
 ?>
